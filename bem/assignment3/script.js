e77419d4c4b1ae11171287861e7ff91aec6fe36b
SharadShakya9 <shakyasharad9@gmail.com>
2024-11-11 14:57:38 +0545

    js applied for all accordion items

--- bem/assignment3/script.js
+++ bem/assignment3/script.js
@@ -1,5 +1,12 @@
-const accordion = document.querySelector(".accordion__item");
+const accordionItems = document.querySelectorAll(".accordion__item");
 
-accordion.addEventListener("click", () => {
-    accordion.classList.toggle("accordion__item--open");
-})
+accordionItems.forEach(item => {
+    const title = item.querySelector(".accordion__title");
+
+    title.addEventListener("click", () => {
+        item.classList.toggle("accordion__item--open");
+
+        const content = item.querySelector(".accordion__copy");
+        content.classList.toggle("accordion__copy--hidden");
+    });
+});
